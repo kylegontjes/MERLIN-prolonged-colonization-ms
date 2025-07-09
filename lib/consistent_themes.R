50cae367eb4a02bad79091ec91ec0064112f7c93
kylegontjes <kgontjes@umich.edu>
2025-07-09 10:16:41 -0400

    update figure numbers again

--- lib/consistent_themes.R
+++ lib/consistent_themes.R
@@ -4,10 +4,10 @@ theme_bw_me <- theme(panel.background = element_rect(fill = "white",colour = NA)
                      panel.grid.major = element_blank(),
                      panel.grid.minor = element_blank(),
                      axis.line = element_line(colour = "black"),legend.position = "bottom") 
-# Figure 2
+# Figure 1
 prolonged_colonization_scale <- scale_fill_manual(breaks = c(1,0),values = c("#00274C","#FFCB05"),labels = c("Yes","No"),name="Prolonged Colonization") 
 
-figure_2_format <- theme(legend.position = "bottom",
+figure_1_format <- theme(legend.position = "bottom",
                            axis.text =   element_text(size=18, color="black"),
                            axis.title = element_text(size = 22, color="black"),
                            legend.text =   element_text(size=20, color="black"),
@@ -17,8 +17,8 @@ figure_2_format <- theme(legend.position = "bottom",
                           legend.margin=margin(c(-20,0,0,0))
 )
 
-# Figure 3
-figure_3_format <- theme(axis.text =   element_text(size=10, color="black"),
+# Figure 2
+figure_2_format <- theme(axis.text =   element_text(size=10, color="black"),
                          axis.title = element_text(size = 12, color="black"),
                          axis.text.x = element_text(angle=90,hjust=0.95,vjust=0.2)
 )
